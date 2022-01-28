ae399b884e337805021b04c9c09a8b72cc137bf2
amir-91 <61394176+amir-91@users.noreply.github.com>
2022-01-28 14:17:08 +0200

    fixing notes

---
+++ __test__/getCityData.spec.js
@@ -0,0 +1,9 @@
+describe("Testing getCityData functionality", () => {
+    test("Testing the getData() function", () => {
+        const city = ''
+        const postalCode = ''
+        const date = ''
+        expect(city).not.toBeNull();
+        expect(postalCode).not.toBeNull();
+        expect(date).not.toBeNull();
+})})
